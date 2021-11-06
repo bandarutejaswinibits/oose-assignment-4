4d70d358ae7edc636daef89d7767ec6eaf369a52
bandarutejaswinibits <2020mt93205@wilp.bits-pilani.ac.in>
2021-11-06 23:02:43 +0530

    Add subtract and multiply functions

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,3 +1,6 @@
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    print("Calling functions from simple.py")
+    print(add_one(5))
+    print(subtract(12, 18))
+    print(multiply(6, 8))
